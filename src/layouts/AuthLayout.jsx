ab0364fda811910fc6ee59077b0a2a3d70c18ef0
Diyorbekkdev <diyorbekjuraev77@gmail.com>
2023-09-24 23:04:07 +0500

    fix: auth responsive

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import { Link, Outlet } from "react-router-dom";
 import logo from "../assets/images/logo.svg";
 import { Language } from "./components/Language";
+import { useTranslation } from "react-i18next";
 
 export const AuthLayout = () => {
+  const {t} = useTranslation();
   return (
     <div className="auth">
       <div className="auth__header d-flex justify-content-between align-items-center">
         <Link className="auth__logo" to={"/"}>
           <img src={logo} alt="logo" />
-          <p>Вактинчалик саклаш хибcхоналаринииг назорат-мониторинги</p>
+          <p>{t('vsx-title')}</p>
         </Link>
         <Language />
       </div>
